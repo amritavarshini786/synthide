0072e5c654ed6eef01515ddbd59b7f05b2beb8a2
A Amrita Varshini <amrita.chinni2005@gmail.com>
2025-06-11 00:03:41 +0530

    more changes to main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ app = FastAPI()
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://synthide.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
